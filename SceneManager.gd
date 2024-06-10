df432aa4470f4edf0504d7c89d2ccc5dd94bada0
treepumpkin <davide.distaso@icloud.com>
2024-06-10 22:14:28 +0200

    Fixes player multiplayer spawner
    Set authority to server on enter_tree, and only spawn if is_server

--- SceneManager.gd
+++ SceneManager.gd
@@ -2,28 +2,32 @@ extends Node2D
 
 @onready var PlayerSpawner_node = get_node("PlayerSpawner")
 
+func _enter_tree():
+	get_node("PlayerSpawner").set_multiplayer_authority(1)
+
 func _ready():
-	var index = 0
-	var spawn_points = get_tree().get_nodes_in_group("PlayerSpawnPoint")
-	for player_id in GameManager.Players:
-		if player_id == 1:
-			continue
-		var currentPlayer = PlayerSpawner_node.spawn()
-		#Getting a breakpoint here because it returns a null ptr
-		currentPlayer.name = str(player_id)
-		
-		if index < spawn_points.size():
-			var spawn = spawn_points[index]
-			currentPlayer.global_position = spawn.global_position
-			print("Assigning player ", player_id, " to spawn point ", index, " at position ", spawn.global_position)
-		
-		# Assign the camera only if this player is the local player
-		if player_id == multiplayer.get_unique_id():
-			var player_camera = currentPlayer.get_node("Camera2D")
-			player_camera.make_current()
-		
-		index += 1
-	pass
+	if multiplayer.is_server():
+		var index = 0
+		var spawn_points = get_tree().get_nodes_in_group("PlayerSpawnPoint")
+		for player_id in GameManager.Players:
+			if player_id == 1:
+				continue
+			var currentPlayer = PlayerSpawner_node.spawn()
+			#Getting a breakpoint here because it returns a null ptr
+			currentPlayer.name = str(player_id)
+			
+			if index < spawn_points.size():
+				var spawn = spawn_points[index]
+				currentPlayer.global_position = spawn.global_position
+				print("Assigning player ", player_id, " to spawn point ", index, " at position ", spawn.global_position)
+			
+			# Assign the camera only if this player is the local player
+			if player_id == multiplayer.get_unique_id():
+				var player_camera = currentPlayer.get_node("Camera2D")
+				player_camera.make_current()
+			
+			index += 1
+		pass
 
 func _process(_delta):
 	pass
