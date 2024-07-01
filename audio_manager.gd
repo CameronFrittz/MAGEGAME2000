da1d6cbafc4d67dc732788f3cf25339de14dc38d
treepumpkin <davide.distaso@icloud.com>
2024-07-01 22:45:16 +0200

    Fixes VOIP

--- audio_manager.gd
+++ audio_manager.gd
@@ -1,52 +1,50 @@
 extends Node
 
-@onready var input : AudioStreamPlayer = $Input
-@export var outputPath : NodePath
-var index : int
-var effect : AudioEffectCapture
-var playback : AudioStreamPlaybackVOIP
-var inputThreshold = 0.00
-var mic_capture: AudioEffectCapture
-var VOIPPlayback : AudioStreamVOIP
+var user = load("res://user.tscn")
+
+var mic_capture: VOIPInputCapture
+
+var users = {} # {Peer ID: AudioStreamPlayer}
+
 var packets_received = 0
 var packets_sent = 0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	setupAudio(get_multiplayer_authority())
+func _ready():
+	multiplayer.peer_connected.connect(_peer_connected)
+	multiplayer.peer_disconnected.connect(_peer_disconnected)
+	
+	var mic_bus = AudioServer.get_bus_index("Mic")
+	mic_capture = AudioServer.get_bus_effect(mic_bus, 0)
+	mic_capture.packet_ready.connect(self._voice_packet_ready)
+	
 
-func setupAudio(id):
-	var VOIP = get_node(outputPath)
-	if VOIP:
-		VOIP.set_multiplayer_authority(id)
-	set_multiplayer_authority(id)
-	if is_multiplayer_authority():
-		var mic = AudioStreamMicrophone.new()
-		input.stream = mic
-		input.play()
-		VOIPPlayback = AudioStreamVOIP.new()
-		VOIP.stream = VOIPPlayback
-		VOIP.play()
-		var mic_bus = AudioServer.get_bus_index("Mic")
-		mic_capture = AudioServer.get_bus_effect(mic_bus, 0)
-		if mic_capture:
-			mic_capture.packet_ready.connect(_on_voice_packet_ready)
+func _peer_connected(id):
+	if id != 1:
+		print("Peer connected with ID ", id)
+		users[id] = user.instantiate()
+		add_child(users[id])
+	
+func _peer_disconnected(id):
+	if id != 1:
+		print("Peer disconnected with ID ", id)
+		users[id].queue_free()
+		users.erase(id)
 
-func _on_voice_packet_ready(packet):
-	if multiplayer.multiplayer_peer.get_connection_status() == MultiplayerPeer.CONNECTION_CONNECTED:
-		_voice_packet_received.rpc(packet)
-		packets_sent += 1
-		print("Packets sent: ", packets_sent)
 
+func _voice_packet_ready(packet):
+	if not multiplayer.is_server():
+		if multiplayer.multiplayer_peer.get_connection_status() == MultiplayerPeer.CONNECTION_CONNECTED:
+			_voice_packet_received.rpc(packet)
+			packets_sent += 1
+			#print("Packets sent: ", packets_sent)
+	
 @rpc("any_peer", "unreliable")
 func _voice_packet_received(packet):
 	packets_received += 1
 	print("Packets received: ", packets_received)
 	var sender_id = multiplayer.get_remote_sender_id()
-	if sender_id == multiplayer.get_unique_id():
-		if VOIPPlayback:
-			VOIPPlayback.push_packet(packet)
+	users[sender_id].stream.push_packet(packet)
+
 
 func _process(_delta):
-	if mic_capture:
-		mic_capture.send_test_packets()
+	mic_capture.send_test_packets()
